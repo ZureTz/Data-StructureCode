da05f17e83c5ef18f17ed35e915e8dc97561c869
Trozure <2819058133@qq.com>
2023-10-21 10:57:37 +0800

    code of problem 3 in test enhanced

--- chapter4/test/3.cc
+++ chapter4/test/3.cc
@@ -12,15 +12,6 @@ int main(int argc, char const *argv[]) {
   cin >> alpha >> beta;
 
   std::string stack;
-  // auto opA = [&]() {
-  //   stack.push_back(alpha.back());
-  //   alpha.pop_back();
-  // };
-  // auto opB = [&]() {
-  //   stack.push_back(beta.back());
-  //   beta.pop_back();
-  // };
-
   bool flag = true;
 
   while (!alpha.empty() && !beta.empty()) {
@@ -32,8 +23,8 @@ int main(int argc, char const *argv[]) {
       if (find != std::string::npos) {
         flag = true;
         alpha.push_back(temp);
-        alpha.append(stack.begin(), stack.begin() + find);
-        stack.erase(stack.begin(), stack.begin() + find);
+        alpha.append(stack.begin(), stack.begin() + find + 1);
+        stack.erase(stack.begin(), stack.begin() + find + 1);
         continue;
       }
 
@@ -44,14 +35,16 @@ int main(int argc, char const *argv[]) {
 
     char temp = beta.front();
     beta.erase(0, 1);
+
     size_t find = stack.find(std::string(1, temp));
     if (find != std::string::npos) {
       flag = false;
       beta.push_back(temp);
-      beta.append(stack.begin(), stack.begin() + find);
-      stack.erase(stack.begin(), stack.begin() + find);
+      beta.append(stack.begin(), stack.begin() + find + 1);
+      stack.erase(stack.begin(), stack.begin() + find + 1);
       continue;
     }
+    
     stack.insert(0, 1, temp);
     flag = !flag;
     continue;
