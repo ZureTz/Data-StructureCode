b5846446b690283ba0dd8a3825b55000c4e6f2ed
TroZure <2819058133@qq.com>
2023-11-08 20:51:10 +0800

    Graph initial implentation

---
+++ chapter7/graph.h
@@ -0,0 +1,128 @@
+#pragma once
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <algorithm>
+#include <functional>
+#include <ostream>
+#include <queue>
+#include <unordered_set>
+#include <vector>
+
+namespace AdjacentMatrix {
+struct Graph {
+  std::vector<std::vector<int>> matrix;
+  Graph(const int n) {
+    matrix.resize(n);
+    std::fill(matrix.begin(), matrix.end(), std::vector<int>(n, 0));
+  }
+  void edgeConnect(const int begin, const int end, const int length) {
+    matrix[begin][end] = length;
+  }
+};
+} // namespace AdjacentMatrix
+
+namespace AdjacentList {
+
+struct EdgeNode {
+  int pointTo;
+  int edgeLength;
+  EdgeNode *nextEdge;
+  EdgeNode(int pointTo, int edgeLength = 1, EdgeNode *nextEdge = nullptr)
+      : pointTo(pointTo), edgeLength(edgeLength), nextEdge(nextEdge) {}
+
+  ~EdgeNode() { delete nextEdge; }
+};
+
+struct VertexNode {
+  int data;
+  EdgeNode *firstEdge;
+  VertexNode(int data, EdgeNode *firstEdge = nullptr)
+      : data(data), firstEdge(firstEdge) {}
+  ~VertexNode() { delete firstEdge; }
+};
+
+struct Graph {
+  std::vector<VertexNode *> list;
+
+  Graph() {}
+
+  void vertexPush(const int data) { list.push_back(new VertexNode(data)); }
+  void edgeConnect(const int begin, const int end, const int edgeLength = 1) {
+    if (begin >= list.size() || end >= list.size()) {
+      return;
+    }
+
+    EdgeNode *beginPtr = list[begin]->firstEdge;
+    if (beginPtr == nullptr) {
+      list[begin]->firstEdge = new EdgeNode(end, edgeLength);
+      return;
+    }
+    while (beginPtr->nextEdge) {
+      if (beginPtr->pointTo == end) {
+        return;
+      }
+      beginPtr = beginPtr->nextEdge;
+    }
+    beginPtr->nextEdge = new EdgeNode(end, edgeLength);
+  }
+
+  void DFS(std::ostream &os, int beginNode) {
+    if (beginNode >= list.size()) {
+      return;
+    }
+    std::unordered_set<VertexNode *> isVisited;
+    std::function<void(VertexNode *)> dfs = [&os, &isVisited, &dfs,
+                                             this](VertexNode *vertex) -> void {
+      if (vertex == nullptr) {
+        return;
+      }
+      if (isVisited.find(vertex) != isVisited.end()) {
+        return;
+      }
+      os << vertex->data << ' ';
+      isVisited.insert(vertex);
+      EdgeNode *node = vertex->firstEdge;
+      while (node) {
+        dfs(list[node->pointTo]);
+        node = node->nextEdge;
+      }
+    };
+    dfs(list[beginNode]);
+  }
+
+  void BFS(std::ostream &os, int beginNode) {
+    if (beginNode >= list.size()) {
+      return;
+    }
+    std::unordered_set<VertexNode *> isVisited;
+    std::queue<VertexNode *> traverseQueue;
+    traverseQueue.push(list[beginNode]);
+
+    while (!traverseQueue.empty()) {
+      VertexNode *node = traverseQueue.front();
+      traverseQueue.pop();
+      if (isVisited.find(node) != isVisited.end()) {
+        continue;
+      }
+      os << node->data << ' ';
+      isVisited.insert(node);
+      EdgeNode *edge = node->firstEdge;
+      while (edge) {
+        traverseQueue.push(list[edge->pointTo]);
+        edge = edge->nextEdge;
+      }
+    }
+  }
+
+  ~Graph() {
+    for (auto ptr : list) {
+      delete ptr;
+    }
+  }
+};
+
+} // namespace AdjacentList
+
+#endif
---
+++ chapter7/graphTest.cc
@@ -0,0 +1,37 @@
+#include "graph.h"
+#include <iostream>
+using std::cout;
+
+int main(int argc, char const *argv[]) {
+  std::ios::sync_with_stdio(false);
+
+  AdjacentList::Graph graph;
+  for (int i = 0; i < 5; ++i) {
+    graph.vertexPush(i);
+  }
+  graph.edgeConnect(0, 1);
+  graph.edgeConnect(1, 2);
+  graph.edgeConnect(2, 0);
+  graph.edgeConnect(2, 4);
+  graph.edgeConnect(4, 3);
+  graph.edgeConnect(3, 1);
+
+  // for (int i = 0; i < 6; ++i) {
+  //   graph.vertexPush(i + 1);
+  // }
+  // graph.edgeConnect(0, 1);
+  // graph.edgeConnect(1, 2);
+  // graph.edgeConnect(1, 3);
+  // graph.edgeConnect(3, 0);
+  // graph.edgeConnect(2, 4);
+  // graph.edgeConnect(4, 5);
+  // graph.edgeConnect(5, 2);
+
+
+  graph.DFS(cout, 0);
+  cout.put('\n');
+  graph.BFS(cout, 0);
+  cout.put('\n');
+
+  return 0;
+}
