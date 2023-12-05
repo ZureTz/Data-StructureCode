c97cfde8e1adabeaf8fc0b9a780f6054e28b21f8
Trozure <2819058133@qq.com>
2023-12-06 02:33:58 +0800

    Course test3 q1 modified

--- course_tests/test3/1.cc
+++ course_tests/test3/1.cc
@@ -12,29 +12,40 @@ int main(int argc, char const *argv[]) {
   (std::cin >> mode >> nStrings).get();
 
   std::vector<std::string> strs(nStrings);
+  for (auto &str : strs) {
+    std::getline(std::cin, str);
+  }
+
   if (mode == 1) {
-    for (auto &str : strs) {
-      std::getline(std::cin, str);
-    }
-  } else {
-    for (auto &ch : toFind) {
-      ch = tolower(ch);
-    }
-    for (auto &str : strs) {
-      std::string temp;
-      std::getline(std::cin, temp);
-      for (auto &ch : temp) {
-        ch = tolower(ch);
+
+    for (const auto &str : strs) {
+      if (str.find(toFind) == std::string::npos) {
+        continue;
       }
-      str = temp;
+      cout << str << std::endl;
+    }
+
+    return 0;
+  }
+
+  std::string tofindLower = toFind;
+  std::vector<std::string> lowerStrs = strs;
+
+  for (auto &ch : tofindLower) {
+    ch = tolower(ch);
+  }
+
+  for (auto & str : lowerStrs) {
+    for (auto &ch : str) {
+      ch = tolower(ch);
     }
   }
 
-  for (const auto&str : strs) {
-    if (str.find(toFind)==std::string::npos) {
+  for (int i = 0; i < lowerStrs.size(); ++i) {
+    if (lowerStrs[i].find(tofindLower) == std::string::npos) {
       continue;
     }
-    cout << str << std::endl;
+    cout << strs[i] << std::endl;
   }
 
   return 0;
